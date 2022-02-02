27647d5e09ddb8e54b534a98d20e9b92ec09ace4
Gabriel <gabriel.agudelo@utp.edu.co>
2022-02-01 20:38:14 -0500

    fecha y fin

--- js/reloj.js
+++ js/reloj.js
@@ -1,10 +1,27 @@
 const mostrarReloj = ()=> {
     let fecha = new Date();
-    let horas = fecha.getHours();
-    let minutos = fecha.getMinutes();
-    let segundos = fecha.getSeconds();
+    let horas = formatoHora(fecha.getHours());
+    let minutos = formatoHora(fecha.getMinutes());
+    let segundos = formatoHora(fecha.getSeconds());
     document.getElementById('hora').innerHTML = `${horas}:${minutos}:${segundos}`;
 
 }
 
+const formatoHora = (hora)=> {
+    if (hora < 10){
+        hora = '0' + hora;
+    }
+    return hora;
+}
+
 setInterval(mostrarReloj, 1000);
+
+
+const mostrarFecha = ()=> {
+
+    let fecha = new Date();
+    let dia = formatoHora(fecha.getDay());
+    let mes = formatoHora(fecha.getMonth());
+    let year = formatoHora(fecha.getFullYear());
+    document.getElementById('fecha').innerHTML = `${dia}/${mes}/${year} </br></br> Día/Mes/año`;
+}
